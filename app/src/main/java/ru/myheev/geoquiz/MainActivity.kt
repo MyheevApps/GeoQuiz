e135beedbec8d1093fac37c3aeeb4752b5f3e963
Maxim Miheev <m.myheev@gmail.com>
2022-07-09 09:00:09 +0300

    add check correct answer

--- app/src/main/java/ru/myheev/geoquiz/MainActivity.kt
+++ app/src/main/java/ru/myheev/geoquiz/MainActivity.kt
@@ -73,7 +73,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer) R.string.correct_toast else R.string.incorrect_toast
+        val messageResId =
+            if (userAnswer == correctAnswer) R.string.correct_toast else R.string.incorrect_toast
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
